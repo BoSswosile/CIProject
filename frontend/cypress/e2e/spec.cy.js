6be3461982c70ac660be2d765cb502888b0bd5f3
Yohan <yohan.moren@gmail.com>
2025-06-25 10:58:58 +0200

    Cypress integration

---
+++ frontend/cypress/e2e/spec.cy.js
@@ -0,0 +1,6 @@
+it('Simple Test', function() {
+  /* ==== Generated with Cypress Studio ==== */
+  cy.visit('localhost:3000');
+  cy.get('.App-link').click();
+  /* ==== End Cypress Studio ==== */
+});
